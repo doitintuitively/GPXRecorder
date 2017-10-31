41ad09518c4f5eca215fe5bc4ac98c6bba4fbe65
Jason Liu <5052067+doitintuitively@users.noreply.github.com>
2017-10-30 22:46:50 -0700

    Conform to Google Java Style.

--- app/src/androidTest/java/com/doitintuitively/gpxrecorder/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/doitintuitively/gpxrecorder/ExampleInstrumentedTest.java
@@ -1,13 +1,12 @@
 package com.doitintuitively.gpxrecorder;
 
+import static org.junit.Assert.assertEquals;
+
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -16,11 +15,11 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+  @Test
+  public void useAppContext() throws Exception {
+    // Context of the app under test.
+    Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.doitintuitively.gpxrecorder", appContext.getPackageName());
-    }
+    assertEquals("com.doitintuitively.gpxrecorder", appContext.getPackageName());
+  }
 }
